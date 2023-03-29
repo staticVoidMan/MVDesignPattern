5efc265156ec305aa7961f5659c14064946f3287
staticVoidMan <staticVoidMan@users.noreply.github.com>
2023-03-29 11:23:53 +0530

    - Initial Commit

---
+++ MVDesignPattern/MVDesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVDesignPatternApp.swift
+//  MVDesignPattern
+//
+//  Created by Amin Siddiqui on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MVDesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MV Design Pattern
+
+MV Design Pattern in iOS - Build SwiftUI Apps Apple's Way
+
+https://www.udemy.com/course/mv-design-pattern-in-ios-for-swiftui

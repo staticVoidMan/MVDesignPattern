062e4915e11c677ef4ed68be7f72a32cb166ab2b
staticVoidMan <staticVoidMan@users.noreply.github.com>
2023-04-07 12:52:11 +0530

    #36

---
+++ Section 5/HelloBudget/HelloBudget/HelloBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBudgetApp.swift
+//  HelloBudget
+//
+//  Created by Amin Siddiqui on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

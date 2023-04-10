7f43c05175c8e37c634c5f948f0fa9054215dab4
staticVoidMan <staticVoidMan@users.noreply.github.com>
2023-04-10 11:47:06 +0530

    #40

--- Section 5/HelloBudget/HelloBudget/ContentView.swift
+++ Section 5/HelloBudget/HelloBudget/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) private var context
+    @FetchRequest(sortDescriptors: []) private var categories: FetchedResults<BudgetCategory>
+    
+    @State private var isPresentingAddNewCategory: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(categories) { category in
+                Text(category.title ?? "")
+            }
+            .sheet(isPresented: $isPresentingAddNewCategory) {
+                AddBudgetCategoryView()
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add new category") {
+                        isPresentingAddNewCategory = true
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, CoreDataProvider.shared.context)
     }
 }
